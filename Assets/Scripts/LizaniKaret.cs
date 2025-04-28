ff0d5d506efd5e2cb30f03808eb9f67d0c3754b8
FiFiliann <fifilianmanik@gmail.com>
2025-04-28 19:28:23 +0200

    RozdavaniKaret

--- Assets/Scripts/LizaniKaret.cs
+++ Assets/Scripts/LizaniKaret.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Unity.VisualStudio.Editor;
 using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor.Sprites;
@@ -16,11 +17,11 @@ public class LizaniKaret : MonoBehaviour
     public OponentUStolu oponentUStolu;
     public manager manager;
     public Sprite[] KartySrdce = new Sprite[13];
-    public Texture[] KartyKary = new Texture[13];
-    public Texture[] KartyPiky = new Texture[13];
-    public Texture[] KartyKrize = new Texture[13];
-    public Texture[] KartySpecialni = new Texture[2];
-
+    public Sprite[] KartyKary = new Sprite[13];
+    public Sprite[] KartyPiky = new Sprite[13];
+    public Sprite[] KartyKrize = new Sprite[13];
+    public Sprite[] KartySpecialni = new Sprite[2];
+    public string posledniKartaOdhazovaciBalicek;
 
     private void Start()
     {
@@ -39,9 +40,9 @@ public class LizaniKaret : MonoBehaviour
             balicek.Add("♣8");
             balicek.Add("♣9");
             balicek.Add("♣10");
-            balicek.Add("♣J");
-            balicek.Add("♣Q");
-            balicek.Add("♣K");
+            balicek.Add("♣11");
+            balicek.Add("♣12");
+            balicek.Add("♣13");
 
             balicek.Add("♦1");
             balicek.Add("♦2");
@@ -53,9 +54,9 @@ public class LizaniKaret : MonoBehaviour
             balicek.Add("♦8");
             balicek.Add("♦9");
             balicek.Add("♦10");
-            balicek.Add("♦J");
-            balicek.Add("♦Q");
-            balicek.Add("♦K");
+            balicek.Add("♦11");
+            balicek.Add("♦12");
+            balicek.Add("♦13");
 
             balicek.Add("♥1");
             balicek.Add("♥2");
@@ -67,9 +68,9 @@ public class LizaniKaret : MonoBehaviour
             balicek.Add("♥8");
             balicek.Add("♥9");
             balicek.Add("♥10");
-            balicek.Add("♥J");
-            balicek.Add("♥Q");
-            balicek.Add("♥K");
+            balicek.Add("♥11");
+            balicek.Add("♥12");
+            balicek.Add("♥13");
 
             balicek.Add("♠1");
             balicek.Add("♠2");
@@ -81,12 +82,12 @@ public class LizaniKaret : MonoBehaviour
             balicek.Add("♠8");
             balicek.Add("♠9");
             balicek.Add("♠10");
-            balicek.Add("♠J");
-            balicek.Add("♠Q");
-            balicek.Add("♠K");
+            balicek.Add("♠11");
+            balicek.Add("♠12");
+            balicek.Add("♠13");
 
-            balicek.Add("J");
-            balicek.Add("J");
+            balicek.Add("J1");
+            balicek.Add("J2");
 
             RozmichaniKaret();
         } //přirazení karet
@@ -95,24 +96,81 @@ public class LizaniKaret : MonoBehaviour
     {
         if (a != null && a.GetComponent<Karta>().LizaciBalicek_Hrac == false)
         {
-            Destroy(coze); Destroy(a); Instantiate(KartaGo, GameObject.Find("HracovaRuka").transform);
+            GameObject kartadoRuky = Instantiate(KartaGo, GameObject.Find("HracovaRuka").transform);
+            PrideleniKarty(kartadoRuky);
+            hracRuka.HracKarty.Add(balicek[0]);
+            balicek.RemoveAt(0);
+            Destroy(coze); 
+            Destroy(a);
         }
     }
     public void KartaProHrace()
     {
         if (a == null)
         {           
-            a = Instantiate(KartaGo, GameObject.Find("LizaciBalicek").transform);            
-            a.GetComponent<Karta>().kartaInfo = balicek[5];
-            GameObject.Find("HracovaRuka").GetComponent<HracRuka>().HracKarty.Add(balicek[5]);
-            balicek.RemoveAt(5);
+            a = Instantiate(KartaGo, GameObject.Find("LizaciBalicek").transform);
             coze = Instantiate(KartaGo, GameObject.Find("HracovaRuka").transform);
-            a.GetComponent<Karta>().LizaciBalicek_Hrac = true;
-            a.GetComponent<Karta>().HracovaRukaPolohaProKartu = coze;
+            PrideleniKarty(a);
         }
     }
-    public void KartyPng()
+    public void PrideleniKarty(GameObject i)
     {
+        i.GetComponent<Karta>().LizaciBalicek_Hrac = true;
+        i.GetComponent<Karta>().HracovaRukaPolohaProKartu = coze;       
+        i.GetComponent<Karta>().ZnackaKarty = balicek[0].Substring(0, 1);        
+        if (balicek[0].Length == 2) { i.GetComponent<Karta>().CisloKarty = int.Parse(balicek[0].Substring(1, 1)); }
+        else { i.GetComponent<Karta>().CisloKarty = int.Parse(balicek[0].Substring(1, 2)); }
+        
+    }
+    public void PocatekHry()
+    {        
+        StartCoroutine(StartKartaOdhozeni());
+        StartCoroutine(StartKartaOponent());
+    }
+    public IEnumerator StartRukaHrace()
+    {
+
+            KartaProHrace();
+            yield return new WaitForSeconds(0.5f);
+    
+    }
+    public IEnumerator StartKartaOdhozeni()
+    {
+        GameObject j = Instantiate(KartaGo, GameObject.Find("LizaciBalicek").transform);
+        PrideleniKarty(j);
+        j.GetComponent<Karta>().LizaciBalicek_Hrac = false;
+        j.GetComponent<Karta>().a = false;
+        j.GetComponent<Karta>().LizaciBalicek_OdhazovaciBalicek = true;
+        posledniKartaOdhazovaciBalicek = balicek[0];
+        balicek.RemoveAt(0);
+        yield return new WaitForSeconds(0.5f);
+    }
+    public IEnumerator StartKartaOponent()
+    {
+        for (int i = 0;i < 4;i++)
+        {
+            for (int j = 0; j < manager.OponentiUStolu.Length; j++)
+            {
+                if (manager.OponentiUStolu[j] != null)
+                {
+                    GameObject a = Instantiate(KartaGo, GameObject.Find("LizaciBalicek").transform);
+                    PrideleniKarty(a);
+                    a.GetComponent<Karta>().LizaciBalicek_Hrac = false;
+                    a.GetComponent<Karta>().a = false;
+                    a.GetComponent<Karta>().LizaciBalicek_Oponent = true;
+                    a.GetComponent<Karta>().ZnackaKarty = "J";
+                    a.GetComponent<Karta>().CisloKarty = 3;
+                    manager.OponentiUStolu[j].GetComponent<OponentUStolu>().OponentKarty.Add(balicek[0]);
+                    manager.OponentiUStolu[j].GetComponent<OponentUStolu>().PocetKaret.text = manager.OponentiUStolu[j].GetComponent<OponentUStolu>().OponentKarty.Count + "";
+
+                    balicek.RemoveAt(0);
+                    a.GetComponent<Karta>().OponentovaRuka = manager.OponentiUStolu[j];
+                    yield return new WaitForSeconds(0.5f);
+                }
+            }
+            StartCoroutine(StartRukaHrace());
+            yield return new WaitForSeconds(0.7f);
+        }
 
     }
     public void RozmichaniKaret()
